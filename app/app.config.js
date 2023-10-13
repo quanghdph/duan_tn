3ae63bd4d9e15a439888296c572a1ec546a77cf3
quanghdph <quanghdph08157@fpt.edu.vn>
2023-10-13 19:47:53 +0700

    update

--- app/app.config.js
+++ app/app.config.js
@@ -1,34 +1,33 @@
-'use strict';
+"use strict";
 
-angular.
-  module('phonecatApp').
-  config(['$routeProvider',
-    function config($routeProvider) {
-      $routeProvider.
-        when('/phones', {
-          template: '<phone-list></phone-list>'
-        }).
-        when('/home', {
-          template: '<home></home>'
-        }).
-        when('/cart', {
-          template: '<cart></cart>'
-        }).
-        when('/delivery', {
-          template: '<delivery></delivery>'
-        }).
-        when('/category', {
-          template: '<category></category>'
-        }).
-        when('/product', {
-          template: '<product></product>'
-        }).
-        when('/payment', {
-          template: '<payment></payment>'
-        }).
-        when('/phones/:phoneId', {
-          template: '<phone-detail></phone-detail>'
-        }).
-        otherwise('/phones');
-    }
-  ]);
+angular.module("phonecatApp").config([
+  "$routeProvider",
+  function config($routeProvider) {
+    $routeProvider
+      .when("/phones", {
+        template: "<phone-list></phone-list>",
+      })
+      .when("/home", {
+        template: "<home></home>",
+      })
+      .when("/cart", {
+        template: "<cart></cart>",
+      })
+      .when("/delivery", {
+        template: "<delivery></delivery>",
+      })
+      .when("/category", {
+        template: "<category></category>",
+      })
+      .when("/product", {
+        template: "<product></product>",
+      })
+      .when("/payment", {
+        template: "<payment></payment>",
+      })
+      .when("/phones/:phoneId", {
+        template: "<phone-detail></phone-detail>",
+      })
+      .otherwise("/home");
+  },
+]);
